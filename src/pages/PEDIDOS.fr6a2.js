21b584595faca1d0d80525f272b98770c9e34243
cosmicdav <71784645+cosmicdav@users.noreply.github.com>
2026-03-09 03:31:35 -0400

    Clean up unused comments in PEDIDOS.fr6a2.js
    Removed commented API references and example code.

--- src/pages/PEDIDOS.fr6a2.js
+++ src/pages/PEDIDOS.fr6a2.js
@@ -1,12 +1,1 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
-$w.onReady(function () {
-import wixWindow from 'wix-window';
-
-$w.onReady(function () {
-    // Al hacer clic, abrimos la simulación de pago
-    $w('#btnRealizarPedido').onClick(() => {
-        wixWindow.openLightbox("VentanaPago"); 
-    });
-});
