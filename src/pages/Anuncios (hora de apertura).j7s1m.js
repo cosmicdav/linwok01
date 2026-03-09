8e5a3c7bdcaff74396ac256dee78abfa34191b55
cosmicdav <71784645+cosmicdav@users.noreply.github.com>
2026-03-09 03:00:55 -0400

    Remove button click handler in Anuncios page
    Remove unused code for handling button click in lightbox.

--- src/pages/Anuncios (hora de apertura).j7s1m.js
+++ src/pages/Anuncios (hora de apertura).j7s1m.js
@@ -1,9 +1,1 @@
-import wixWindow from 'wix-window';
 
-$w.onReady(function () {
-    // Cuando el usuario termina de poner sus datos y da clic
-    $w('#btnFinalizarTarjeta').onClick(() => {
-        // Cerramos la ventana y mandamos la señal de "PAGO_OK" a la página principal
-        wixWindow.lightbox.close("PAGO_OK");
-    });
-});
